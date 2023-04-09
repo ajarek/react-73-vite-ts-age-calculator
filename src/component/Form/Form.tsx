84fc8d0f3052a9f3edb6a1733f586d4a53136637
Ajarek <ajarek@poczta.onet.pl>
2023-04-10 00:42:39 +0200

    initial commit

---
+++ src/component/Form/Form.tsx
@@ -0,0 +1,69 @@
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import './Form.css'
+
+type Data = {
+  day: number
+  month: number
+  year: number
+}
+export const Form = () => {
+  const schema = yup.object().shape({
+    day: yup.number().min(1).max(31).required('Your day is Required!'),
+    month: yup.number().min(1).max(12).required('Your day is Required!'),
+    year: yup.number().min(1000).max(2023).required('Your day is Required!'),
+   
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Data>({
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit = (data: Data): void => {
+    alert('Thank you ' + data.day + ' ' + data.month + ' ' + data.year)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="wrapper-all-input">
+      <div className="wrapper-input">
+      <label htmlFor="day">DAY</label>
+      <input
+        type='text'
+        placeholder='DD'
+        {...register('day')}
+      />
+      <p>{errors?.day?.message}</p>
+      </div>
+      <div className="wrapper-input">
+      <label htmlFor="month">MONTH</label>
+      <input
+        type='text'
+        placeholder='MM'
+        {...register('month')}
+      />
+      <p>{errors?.month?.message}</p>
+      </div>
+      <div className="wrapper-input">
+      <label htmlFor="year">YEAR</label>
+      <input
+        type='text'
+        placeholder='YYYY'
+        {...register('year')}
+      />
+      <p>{errors?.year?.message}</p>
+      </div>
+      </div>
+      <div className="submit">
+      <button type='submit'>
+        <img src="/icon-arrow.svg" alt="icon" />
+      </button>
+      </div>
+    </form>
+  )
+}
