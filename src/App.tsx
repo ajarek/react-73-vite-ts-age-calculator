b99ad1c75f301134f0d322ef86e65bf0bebdc3dd
Ajarek <ajarek@poczta.onet.pl>
2023-04-10 21:22:47 +0200

    added component Result

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import {Form } from './component/Form/Form'
+import Result  from './component/Result/Result'
 function App() {
   
 
   return (
     <div className="App">
       <Form/>
-      
+      <Result/>
     </div>
   )
 }
---
+++ src/component/Result/Result.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Result.css'
+const Result = () => {
+  return (
+    <div className='result'>
+      <div className="years"><span>38</span> years</div>
+      <div className="months"><span>15</span> months</div>
+      <div className="days"><span>12</span> days</div>
+    </div>
+  )
+}
+
+export default Result
